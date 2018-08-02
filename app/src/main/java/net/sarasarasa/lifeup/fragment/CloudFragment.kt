4ab4aca6537ccdd2c17437c8b811f024ce39a2ed
Ayagikei <ayagikei@163.com>
2018-08-03 03:12:40 +0800

    tabs for cloud fragment

--- app/src/main/java/net/sarasarasa/lifeup/fragment/CloudFragment.kt
+++ app/src/main/java/net/sarasarasa/lifeup/fragment/CloudFragment.kt
@@ -1,17 +1,92 @@
 package net.sarasarasa.lifeup.fragment
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_comm.view.*
 import net.sarasarasa.lifeup.R
 
 class CloudFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_cloud, null)
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    override fun onCreateView(inflater: LayoutInflater, mContainer: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+        val view = inflater.inflate(R.layout.fragment_cloud, null)
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = SectionsPagerAdapter(childFragmentManager)
+
+        // Set up the ViewPager with the sections adapter.
+        val viewPager = view.findViewById<ViewPager>(R.id.container)
+        val tabs = view.findViewById<TabLayout>(R.id.tabs)
+        viewPager.adapter = mSectionsPagerAdapter
+
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(viewPager))
+
+        return view
     }
 
 
+    /**
+     * A [FragmentPagerAdapter] that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            // Show 3 total pages.
+            return 3
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    class PlaceholderFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val rootView = inflater.inflate(R.layout.fragment_comm, container, false)
+            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
+            return rootView
+        }
+
+        companion object {
+            /**
+             * The fragment argument representing the section number for this
+             * fragment.
+             */
+            private val ARG_SECTION_NUMBER = "section_number"
+
+            /**
+             * Returns a new instance of this fragment for the given section
+             * number.
+             */
+            fun newInstance(sectionNumber: Int): PlaceholderFragment {
+                val fragment = PlaceholderFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
+
+
+    }
 }
